fb07a587083c40dd9b382b5f68928bc591f83bf8
Luan Bairro <luandebairro@gmail.com>
2021-04-05 21:11:41 -0300

    Conectando ao mongo compass

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -7,5 +7,7 @@ const DevSchema =  new mongoose.Schema({
     avatar_url: String,
     techs: [String], //Criando um vetor de Strings, a aplicação vai entender que nesse campo vão ter uma ou mais strings.
 
-    //Pausado aos 54 minutos. Vídeo 2
+
 });
+
+module.exports  = mongoose.model('Dev', DevSchema); //Exporta o model para o mongoose, para poder utilizar a geolocalizacao. Dev eh o nome como vai ser salvo no banco de dados
